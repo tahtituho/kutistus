22801b9a81a3ab155c11d52060161ed351a66052
Arttu Tamminen <arttu.tamminen@kapsi.fi>
2025-02-12 21:53:34 +0200

    initial release size 1169

---
+++ main.js
@@ -0,0 +1,65 @@
+c = document.body.appendChild(document.createElement`canvas`);
+c.width = 1280;
+c.height = 720;
+
+for (i in g = c.getContext`webgl`) {
+    g[i[0] + i[6]] = g[i];
+}
+
+for (i in a = new AudioContext) {
+    a[i[6]] = a[i];
+}
+
+cs = (src, type, shader = g.cS(type)) => {
+    g.sS(shader, src);
+    g.compileShader(shader);
+    return shader;
+}
+
+d = _ => {  
+    g.uniform1f(g.gf(p, "t"), a.currentTime);
+    g.dr(6, 0, 3);
+    requestAnimationFrame(d);
+}
+
+c.onclick = _ => {
+    c.requestFullscreen();
+    g.aS(p = g.cP(), cs(`attribute vec4 p;void main(){gl_Position=p;}`, 35633));
+    g.aS(p, cs(shader_frag, 35632));
+
+    g.lo(p);
+
+    g.vA(g.ug(p), 2, 5120, g.bf(34962, g.cB()), 1, g.bD(34962, new Int8Array([1, -3, 1, 1]), 35044));
+    g.eV(0);
+    
+    c = a.B();
+    c.buffer = a.createBuffer(1, q = 44000 * 45, 44000);
+    for (t = q; t--;) {
+        //Write tune here
+        c.buffer.getChannelData(0)[t] = 
+        (_ => {
+            var fract = x => x % 1;
+            var time = t / 48000;
+            var m = Math;
+            var mix = (a, b, c) => (a * (1 - c)) + (b * c);
+            var noise = x => m.sin((x + 10) * m.sin(m.pow(x + 10, fract(x) + 10)));
+            
+            var bassmelodygen = function(time) {
+                var melody = 0;
+                for(let i = 0; i < 6; i++) {
+                    melody += m.asin(
+                        m.sin(time * mix(50.0 + i, i * 50.0, '67886654'.charAt(m.floor(time * 4) % 8))) * (1 - fract(time * 4))) / m.PI;
+                }
+                return melody;
+            };
+            var hihat = noise(time) * m.pow(1 - fract(time * 4), 3) / 4;
+            var kick = m.sin(m.pow(1 - fract(time + 0.5), 5) * 200);
+            var snare = noise(m.floor(time * 2000)) * m.pow(1 - fract(time), 5) / 2;
+
+            return (bassmelodygen(time) ** 3) + (hihat + kick + snare) / 2;
+        })();
+    }
+    c.connect(a.a);
+    c.start();
+    d();
+}
---
+++ shader.frag
@@ -0,0 +1,44 @@
+precision lowp float;
+uniform float t;
+vec2 resolution = vec2(1280, 720);
+float d;
+float mt;
+
+vec3 as;
+vec3 ac;
+
+vec2 uv;
+
+vec3 rot(vec3 zp, vec3 a) {
+    as = sin(a);
+    ac = cos(a);
+    zp = vec3(
+        ac.z*zp.x-as.z*zp.y,
+        as.z*zp.x+ac.z*zp.y,
+        zp.z);
+    return vec3(
+        ac.y*zp.x+as.y*zp.z,
+        zp.y,
+        -as.y*zp.x+ac.y*zp.z
+    );
+}
+
+float scene(vec3 path) {    
+    path = rot(path, vec3(-t)) + sin(rot(path, vec3(t * 2.0))) * 0.3;
+    mt = length(tan(path + t));
+    vec4 p = vec4(path, 1.0);
+    for (int i = 0; i < 12; i++) {
+        p.xyz = clamp(p.xyz, -2.5, 2.5) * 2.0 - p.xyz; 
+        float r = dot(p.xyz, p.xyz);
+        p *= ((r < 6.0) ? 6.0 / r : 1.0) * (-29.8 * 0.272321 + 5.0);
+    }
+    return length(p.xyz) / abs(p.w);
+}
+
+void main() {
+    uv = (2.0 * gl_FragCoord.xy - resolution) / resolution.y;
+    for(int i = 0; i <= 64; i++) {
+        d += scene(vec3(0.5) + normalize(vec3(0.0, -0.5145, -0.8575) + uv.x * vec3(-0.75, 0.0, 0.0) + uv.y * vec3(0.0, 0.6443, -0.3859)) * d);
+    }
+    gl_FragColor = vec4(vec3(smoothstep(d, 0.0, 2.0)) * (mt * vec3(0.94, 0.81, 0.05)), 1.0); 
+}
